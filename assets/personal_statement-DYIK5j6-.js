1348072e0d7491b96b186f7092d40a356a7071a7
chrislevyli <chris.levy@lendinvest.com>
2025-05-18 11:13:07 +0000

    Updates

---
+++ assets/personal_statement-DYIK5j6-.js
@@ -0,0 +1,6 @@
+const e=`_#_ A Lead/Principal Software Engineer with over 20 years experience, always looking to further expand his skills and knowledge.
+
+_#_ Reliable, trustworthy, versatile and hardworking, with the ability to work either independently or as part of a team.
+
+_#_ Fully set up to work remotely from home.
+`;export{e as default};
