73de81146abac3561a61c6cba98f884a1b4dd825
chrislevyli <chris.levy@lendinvest.com>
2025-05-18 16:03:17 +0000

    Updates

---
+++ assets/interests-jyD-A2EU.js
@@ -0,0 +1,8 @@
+const n=`_#_ I enjoy playing social computer games, both offline and online.
+
+_#_ I regularly play Magic: The Gathering socially, both online and in-person at local game stores.
+
+_#_ I also enjoy other activities including swimming and scuba diving.
+
+_#_ I have attended PHP User Groups to learn more about Development, and I have spoken at the PHP London User Group and PHP UK Conference in 2015.
+`;export{n as default};
