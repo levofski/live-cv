19fa240baa190e83c157e7daf7c8cc811ba3e8bf
chrislevyli <chris.levy@lendinvest.com>
2025-05-20 06:35:00 +0000

    Updates

---
+++ assets/personal_statement-DHM_UEA0.js
@@ -0,0 +1,8 @@
+const e=`_#_ A Lead/Staff Software Engineer with over 20 years experience, always looking to further expand his skills and knowledge.
+
+_#_ Reliable, trustworthy, versatile and hardworking, with the ability to work either independently or as part of a team.
+
+_#_ I believe my greatest skill is being able to pick up and learn new skills and tools very quickly.
+
+_#_ Fully set up to work remotely from home.
+`;export{e as default};
